641d1c2484d14cd78fd849bcd8bd2e0362eda0c5
rpissardo <rpissardo@hotmail.com>
2017-08-14 23:19:17 -0300

    Traduzindo o processo

--- simple_decision_tree/decision_tree.py
+++ simple_decision_tree/decision_tree.py
@@ -14,9 +14,9 @@ from sklearn.tree import DecisionTreeClassifier
 n_classes = 3
 n_estimators = 30
 cmap = plt.cm.RdYlBu
-plot_step = 0.02  # fine step width for decision surface contours
-plot_step_coarser = 0.5  # step widths for coarse classifier guesses
-RANDOM_SEED = 13  # fix the seed on each iteration
+plot_step = 0.02  # Superficie de Contorno
+plot_step_coarser = 0.5  # Largura do Passo
+RANDOM_SEED = 13  # Seed em cada iteracao
 
 # Load data
 iris = load_iris()
@@ -32,29 +32,30 @@ for dataset_loaded in datasets_loaded:
     dataset_count += 1
     for pair in ([0, 1], [0, 2], [2, 3], [1, 2], [1, 3]):
         for model in models:
-            # We only take the two corresponding features
+            # Pegamos apenas as caracteriscas correspondentes
             X = dataset_loaded.data[:, pair]
             y = dataset_loaded.target
 
-            # Shuffle
+            # Randomizando
             idx = np.arange(X.shape[0])
             np.random.seed(RANDOM_SEED)
             np.random.shuffle(idx)
             X = X[idx]
             y = y[idx]
 
-            # Standardize
+            # Normalizando
             mean = X.mean(axis=0)
             std = X.std(axis=0)
             X = (X - mean) / std
 
-            # Train
+            # Treinamento
             clf = clone(model)
             clf = model.fit(X, y)
 
+            # Pontuacao
             scores = clf.score(X, y)
-            # Create a title for each column and the console by using str() and
-            # slicing away useless parts of the string
+            
+            # Determinando titulos
             model_title = str(type(model)).split(
                 ".")[-1][:-2][:-len("Classifier")]
 
@@ -67,37 +68,33 @@ for dataset_loaded in datasets_loaded:
 
             plt.subplot(5, 3, plot_idx)
             if plot_idx <= 5*dataset_count:
-                # Add a title at the top of each column
+                # Adicionando Titulos
                 plt.title(datasets_loaded_name[dataset_count-1])
 
-            # Now plot the decision boundary using a fine mesh as input to a
-            # filled contour plot
+            # Limite de decisao de acordo com a malha
             x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
             y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
             xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                                  np.arange(y_min, y_max, plot_step))
 
-            # Plot either a single DecisionTreeClassifier or alpha blend the
-            # decision surfaces of the ensemble of classifiers
+            # Plotar
             if isinstance(model, DecisionTreeClassifier):
                 Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
                 Z = Z.reshape(xx.shape)
                 cs = plt.contourf(xx, yy, Z, cmap=cmap)
             else:
-                # Choose alpha blend level with respect to the number
-                # of estimators
-                # that are in use (noting that AdaBoost can use fewer estimators
-                # than its maximum if it achieves a good enough fit early on)
+                # Escolha o nivel de mistura de alfa em relacao ao numero de estimadores 
+                # em uso (observando que o AdaBoost pode usar menos estimadores do que o 
+                # maximo se atingir um ajuste suficientemente bom no inicio)
                 estimator_alpha = 1.0 / len(model.estimators_)
                 for tree in model.estimators_:
                     Z = tree.predict(np.c_[xx.ravel(), yy.ravel()])
                     Z = Z.reshape(xx.shape)
                     cs = plt.contourf(xx, yy, Z, alpha=estimator_alpha, cmap=cmap)
 
-            # Build a coarser grid to plot a set of ensemble classifications
-            # to show how these are different to what we see in the decision
-            # surfaces. These points are regularly space and do not have a
-            # black outline
+            # Crie uma grade mais grosseira para tracar um conjunto de classificacoes de 
+            # conjunto para mostrar como estas sao diferentes do que vemos nas areas de decisao. 
+            # Esses pontos sao regularmente espaco e nao tem um esboco preto
             xx_coarser, yy_coarser = np.meshgrid(
                 np.arange(x_min, x_max, plot_step_coarser),
                 np.arange(y_min, y_max, plot_step_coarser))
@@ -108,12 +105,11 @@ for dataset_loaded in datasets_loaded:
                                     c=Z_points_coarser, cmap=cmap,
                                     edgecolors="none")
 
-            # Plot the training points, these are clustered together and have a
-            # black outline
+            # Plotar os pontos de treinamento
             plt.scatter(X[:, 0], X[:, 1], c=y,
                         cmap=ListedColormap(['r', 'y', 'b']),
                         edgecolor='k', s=20)
-            plot_idx += 1  # move on to the next plot in sequence
+            plot_idx += 1  # Proxima sequencia
 plt.suptitle("Classifiers on feature subsets")
 plt.tight_layout()
 
