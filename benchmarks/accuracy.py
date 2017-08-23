eeccd2cf182dd525383e09e0ebaf754491221e79
Felipe Anchieta Santos Costa <felipeanchieta@hotmail.com.br>
2017-08-23 00:31:32 -0300

    Added classifiers accuracy benchmark

--- benchmarks/accuracy.py
+++ benchmarks/accuracy.py
@@ -8,19 +8,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-# load dataset
+from sklearn.datasets import load_breast_cancer, load_iris, load_wine
 from extra_trees.ensemble.forest import ExtraTreesClassifier
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-dataframe = pandas.read_csv(url, names=names)
-array = dataframe.values
-X = array[:, 0:8]
-Y = array[:, 8]
-# prepare configuration for cross validation test harness
-seed = 7
 # prepare models
-models = [
+classification_models = [
     ('Logistic', LogisticRegression()),
     ('Nearest Neighbors', KNeighborsClassifier()),
     ('SVM', SVC()),
@@ -29,20 +21,69 @@ models = [
     ('ExtraTrees (SciKit)', SKExtraTreesClassifier()),
     ('ExtraTrees', ExtraTreesClassifier()),
 ]
+seed = 7
+
+print("breast_cancer")
+breast_cancer = load_breast_cancer()
+X, y = breast_cancer.data, breast_cancer.target
 # evaluate each model in turn
 results = []
 names = []
 scoring = 'accuracy'
-for name, model in models:
+for name, model in classification_models:
     kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+    cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
 # boxplot algorithm comparison
 fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
+fig.suptitle('breast_cancer')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+print("iris")
+iris = load_iris()
+X, y = iris.data, iris.target
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in classification_models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('iris')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+print("wine")
+wine = load_wine()
+X, y = wine.data, wine.target
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in classification_models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('wine')
 ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(names)
