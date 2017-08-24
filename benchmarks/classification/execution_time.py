878f401e2c38c32b327f52a8b9a8376000bbbf1a
Felipe Anchieta Santos Costa <felipeanchieta@hotmail.com.br>
2017-08-23 22:53:24 -0300

    Added time measures

---
+++ benchmarks/classification/execution_time.py
@@ -0,0 +1,53 @@
+# From MCZA015-13 class notes, modified by Felipe Anchieta Santos Costa
+# License: BSD Style.
+
+import time
+
+import numpy as np
+from sklearn.datasets import load_breast_cancer, load_iris, load_wine
+from sklearn.ensemble import ExtraTreesClassifier as SKExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+from extra_trees.ensemble.forest import ExtraTreesClassifier
+
+
+classification_models = [
+    ('SVC', SVC()),
+    ('RandomForest', RandomForestClassifier()),
+    ('ExtraTrees (SciKit)', SKExtraTreesClassifier()),
+    ('ExtraTrees', ExtraTreesClassifier()),
+]
+
+
+classification_data_sets = [
+    ('breast_cancer', load_breast_cancer(return_X_y=True)),
+    ('iris', load_iris(return_X_y=True)),
+    ('wine', load_wine(return_X_y=True)),
+]
+
+
+for data_name, data_set in classification_data_sets:
+
+    print("{}\n".format(data_name) + '*' * len(data_name))
+    X, y = data_set
+
+    train_size = (len(X) // 4) * 3  # ~75% for training
+    test_size = len(X) - train_size # ~25% for testing
+
+    # do some random magic stuff
+    fx = np.arange(len(X))
+    np.random.shuffle(fx)
+
+    for name, model in classification_models:
+        print("model: {}\n=======".format(name) + "=" * len(name))
+
+        train = X[fx[0:train_size],:]
+        test = X[fx[train_size:],:]
+
+        start = time.time()
+        model.fit(train, y[fx[0:train_size]])
+        model.predict(test)
+        print("score={}".format(model.score(test, y[fx[train_size:]])))
+        end = time.time()
+        print("elapsed time={}\n".format(end - start))
---
+++ benchmarks/regression/execution_time.py
@@ -0,0 +1,52 @@
+# From MCZA015-13 class notes, modified by Felipe Anchieta Santos Costa
+# License: BSD Style.
+
+import time
+
+import numpy as np
+from sklearn.datasets import load_boston, load_diabetes
+from sklearn.ensemble import ExtraTreesRegressor as SKExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
+from extra_trees.ensemble.forest import ExtraTreesRegressor
+
+
+regression_models = [
+    ('SVR', SVR()),
+    ('RandomForest', RandomForestRegressor()),
+    ('ExtraTrees (SciKit)', SKExtraTreesRegressor()),
+    ('ExtraTrees', ExtraTreesRegressor())
+]
+
+
+regression_data_sets = [
+    ('boston', load_boston(return_X_y=True)),
+    ('diabetes', load_diabetes(return_X_y=True))
+]
+
+
+for data_name, data_set in regression_data_sets:
+
+    print("{}\n".format(data_name) + '*' * len(data_name))
+    X, y = data_set
+
+    train_size = (len(X) // 4) * 3  # ~75% for training
+    test_size = len(X) - train_size # ~25% for testing
+
+    # do some random magic stuff
+    fx = np.arange(len(X))
+    np.random.shuffle(fx)
+
+    for name, model in regression_models:
+        print("model: {}\n=======".format(name) + "=" * len(name))
+
+        train = X[fx[0:train_size],:]
+        test = X[fx[train_size:],:]
+
+        start = time.time()
+        model.fit(train, y[fx[0:train_size]])
+        model.predict(test)
+        print("score={}".format(model.score(test, y[fx[train_size:]])))
+        end = time.time()
+        print("elapsed time={}\n".format(end - start))
